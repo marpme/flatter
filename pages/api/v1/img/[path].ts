52eeb0cfd47d97817c38206d7ccc5f1df3dbd4a9
marpme <7430964+marpme@users.noreply.github.com>
2022-08-30 19:04:19 +0200

    return 404 if link couldn't be resolved

--- pages/api/v1/img/[path].ts
+++ pages/api/v1/img/[path].ts
@@ -2,9 +2,12 @@ import { NextApiResponse } from 'next'
 import { decryptImage } from '../../../../lib/crypto'
 
 export default async (req, res: NextApiResponse) => {
-    if (req.query.path) {
-        const imageToLoad = decryptImage(req.query.path)
+    if (!req.query.path) {
+        res.status(404).send(null)
+    }
 
+    try {
+        const imageToLoad = decryptImage(req.query.path)
         const imageResponse = await fetch(imageToLoad)
         const imageType = await imageResponse.headers.get('Content-Type')
         const imageContent = await imageResponse.arrayBuffer()
@@ -12,8 +15,8 @@ export default async (req, res: NextApiResponse) => {
         res.status(200)
             .setHeader('Content-Type', imageType)
             .send(Buffer.from(imageContent))
-        return
+    } catch (e) {
+        console.error(e)
+        res.status(500).send(null)
     }
-
-    res.status(200).send(null)
 }
