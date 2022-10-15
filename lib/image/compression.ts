20a911d788ed8db970f3473374e69af6c87e0b98
marpme <7430964+marpme@users.noreply.github.com>
2022-10-15 18:38:21 +0200

    fix(img): improve image performance compression and cache

--- lib/image/compression.ts
+++ lib/image/compression.ts
@@ -1,14 +1,12 @@
 import sharp from 'sharp'
 
-const config = {
-    jpeg: { quality: 80 },
-    webp: { quality: 80 },
-    png: {compressionLevel: 8},
-}
-
 export const compressImage = async (content: ArrayBuffer) => {
     const image = sharp(Buffer.from(content))
-    const meta = await image.metadata()
-    const { format } = meta
-    return image[format](config[format]).toBuffer()
+
+    return image
+        .png({compressionLevel: 6})
+        .jpeg({ quality: 60 })
+        .webp({ quality: 60 })
+        .resize(500)
+        .toBuffer()
 }
--- pages/api/v1/img/[path].ts
+++ pages/api/v1/img/[path].ts
@@ -4,24 +4,37 @@ import {compressImage} from "../../../../lib/image/compression";
 
 export const config = {
     api: {
-        responseLimit: '4.5mb',
+        responseLimit: '3mb',
     },
 }
+
+const cacheMap = new Map()
 
 export default async (req, res: NextApiResponse) => {
     if (!req.query.path) {
         res.status(404).send(null)
     }
 
+    res.setHeader('Cache-Control', 's-maxage=86400');
+
     try {
         const imageToLoad = decryptImage(req.query.path)
+        if(cacheMap.has(imageToLoad)) {
+            const {imageType, imageContent} = cacheMap.get(imageToLoad)
+            res.status(200)
+                .setHeader('Content-Type', imageType)
+                .send(imageContent)
+            return
+        }
+
         const imageResponse = await fetch(imageToLoad)
         const imageType = await imageResponse.headers.get('Content-Type')
-        const imageContent = await imageResponse.arrayBuffer()
+        const imageContent = await compressImage(await imageResponse.arrayBuffer())
 
+        cacheMap.set(imageToLoad, { imageType, imageContent })
         res.status(200)
             .setHeader('Content-Type', imageType)
-            .send(await compressImage(imageContent))
+            .send(imageContent)
     } catch (e) {
         console.error(e)
         res.status(500).send(null)
