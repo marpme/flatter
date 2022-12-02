2bb466f4697f620323a4e3596b69624e02a3108a
Marvin <7430964+marpme@users.noreply.github.com>
2022-12-02 18:22:53 +0100

    fix: pathing and additional sec headers

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@ module.exports = {
   async headers() {
     return [
       {
-        source: "/*",
+        source: "/:path*",
         headers: [
           {
             key: "Content-Security-Policy",
@@ -21,6 +21,19 @@ module.exports = {
             key: "X-XSS-Protection",
             value: "1; mode=block",
           },
+          {
+            key: 'X-DNS-Prefetch-Control',
+            value: 'on'
+          },
+          {
+            key: 'Strict-Transport-Security',
+            value: 'max-age=63072000; includeSubDomains; preload'
+          },
+          {
+            key: 'Referrer-Policy',
+            value: 'origin-when-cross-origin'
+          },
+          
         ],
       },
     ];
