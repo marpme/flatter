041429f6c372387cab9719cc90341bc51819723e
Marvin <7430964+marpme@users.noreply.github.com>
2022-12-02 17:45:01 +0100

    chore: add security related headers

---
+++ next.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  async headers() {
+    return [
+      {
+        source: "*",
+        headers: [
+          {
+            key: "Content-Security-Policy",
+            value:
+              "default-src 'self'; script-src 'self'; style-src 'self'; img-src 'self'; font-src 'self'; connect-src 'self'; media-src 'none'; object-src 'none'; prefetch-src 'self'; frame-ancestors 'none'",
+          },
+          {
+            key: "X-Content-Type-Options",
+            value: "nosniff",
+          },
+          {
+            key: "X-Frame-Options",
+            value: "DENY",
+          },
+          {
+            key: "X-XSS-Protection",
+            value: "1; mode=block",
+          },
+        ],
+      },
+    ];
+  },
+};
