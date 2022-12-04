65afba9b1016251b2bc4b286127ed8b6abd59d17
marpme <7430964+marpme@users.noreply.github.com>
2022-12-04 09:48:23 +0100

    chore: remove obsolete provider based fetching
    Signed-off-by: marpme <7430964+marpme@users.noreply.github.com>

--- components/property/PropertyLoader.ts
+++ components/property/PropertyLoader.ts
@@ -1,27 +1,5 @@
 import Property from '../../types/Property'
 
-export const loadAvailablePropertyProvider = async (): Promise<string[]> => {
-    return (
-        await fetch('/api/v1/properties/available', {
-            headers: {
-                Accept: 'application/json',
-            },
-        })
-    ).json()
-}
-
-export const loadPropertyByProvider = async (
-    provider: string
-): Promise<Property[]> => {
-    return (
-        await fetch(`/api/v1/properties/${provider}`, {
-            headers: {
-                Accept: 'application/json',
-            },
-        })
-    ).json()
-}
-
 export const loadProperties = async (): Promise<Property[]> => {
     return (
         await fetch(`/api/v1/properties`, {
